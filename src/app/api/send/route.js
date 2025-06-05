a00a2a79a484a597e8b6384adb6cb8debb4ebb7e
Vuong Ngoc Hue <ngochuevuong12@gmail.com>
2025-06-05 18:17:19 +0700

    run

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -4,25 +4,30 @@ import { Resend } from "resend";
 const resend = new Resend(process.env.RESEND_API_KEY);
 const fromEmail = process.env.FROM_EMAIL;
 const toEmail = process.env.TO_EMAIL;
-export async function POST(req, res) {
-  const { email, subject, message } = await req.json();
-  console.log(email, subject, message);
+
+export async function POST(req) {
   try {
+    const { email, subject, message } = await req.json();
+
+    console.log("Send email data:", { email, subject, message });
+
     const data = await resend.emails.send({
       from: fromEmail,
       to: [toEmail, email],
       subject: subject,
       react: (
         <>
-          <h1>{subject}  {email}</h1>
+          <h1>{subject} - {email}</h1>
           <p>Thank you for contacting us!</p>
           <p>New message submitted:</p>
           <p>{message}</p>
         </>
       ),
     });
+
     return NextResponse.json(data);
   } catch (error) {
-    return NextResponse.json({ error });
+    console.error("Error sending email:", error);
+    return NextResponse.json({ error: error.message || "Unknown error" }, { status: 500 });
   }
 }
